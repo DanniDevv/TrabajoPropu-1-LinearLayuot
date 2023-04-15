9b5e726a7ea2b560d4bc9d58ceb1c23424b5cd75
Dante Rodríguez <dante.rodriguez.c@tecsup.edu.pe>
2023-04-15 17:40:06 -0500

    TrabajoPropuesto-LinearLayout

---
+++ app/src/main/java/com/miempresa/trabajo_propuesto_4/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.miempresa.trabajo_propuesto_4
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
